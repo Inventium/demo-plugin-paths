86c32a433ed53995ba57ce4c767a6e034e8ae4c0
Dave Doolin <david.doolin@gmail.com>
2011-03-11 13:04:05 -0800

    Changed to rational license, added a link to current home page.

--- demo-plugin-paths.php
+++ demo-plugin-paths.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: Demo Plugin Paths
- Plugin URI: http://website-in-a-weekend.net/plugins/demo-plugins/
+ Plugin URI: http://website-in-a-weekend.net/demo-plugins/demo-plugin-paths/
  Description: A brief description of the Plugin.
  Version: 0.1
  Author: Dave Doolin
@@ -9,17 +9,7 @@
  */
 
 /*  Copyright 2009 David M. Doolin  (email : david.doolin@gmail.com)
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Public domain.  Use this code. Make billions. Pay it forward.
  */
 
 
